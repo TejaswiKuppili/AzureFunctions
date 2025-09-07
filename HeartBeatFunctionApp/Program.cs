1b65bc6a435fec77ded00d60437e4463344710ef
Tejaswi Kuppili <tejaswi.kuppili@cognine.com>
2025-09-07 13:47:46 +0530

    Durable Orchestration Function Dependency Updated

--- HeartBeatFunctionApp/Program.cs
+++ HeartBeatFunctionApp/Program.cs
@@ -1,12 +1,20 @@
 using Azure.Storage.Blobs;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Builder;
+using Microsoft.DurableTask.Worker;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 var builder = FunctionsApplication.CreateBuilder(args);
 
 builder.ConfigureFunctionsWebApplication();
+
+// Register Durable Task extension (no HubName here)
+builder.Services.AddDurableTaskWorker(options =>
+{
+    //options.UseStorage(new AzureStorageDurabilityProviderFactory());
+    options.UseGrpc();
+});
 
 builder.Services
     .AddApplicationInsightsTelemetryWorkerService()
